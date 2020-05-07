PlatformLab/NanoLog

commit c6efd373c4736427b197312e1bbee504b3ba9277

Upgrade scripts to python3 and fix up some undefined behaviour and gcc 9 warnings

runtime/NanoLogCpp17.h
@@ -376,8 +376,7 @@ store_argument(char **storage,
                ParamType paramType,
                size_t stringSize)
 {
-    T *dest = reinterpret_cast<T*>(*storage);
-    *dest = arg;
+    std::memcpy(*storage, &arg, sizeof(T));
     *storage += sizeof(T);
 
     #ifdef ENABLE_DEBUG_PRINTING
@@ -410,18 +409,22 @@ store_argument(char **storage,
     // Since we've already paid the cost to find the string length earlier,
     // might as well save it in the stream so that the compression function
     // can later avoid another strlen/wsclen invocation.
-    uint32_t *size = reinterpret_cast<uint32_t*>(*storage);
+    if(stringSize > std::numeric_limits<uint32_t>::max())
+    {
+        throw std::invalid_argument("Strings larger than std::numeric_limits<uint32_t>::max() are unsupported");
+    }
+    auto size = static_cast<uint32_t>(stringSize);
+    std::memcpy(*storage, &size, sizeof(uint32_t));
     *storage += sizeof(uint32_t);
-    *size = static_cast<uint32_t>(stringSize);
 
 #ifdef ENABLE_DEBUG_PRINTING
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wpointer-arith"
 #pragma GCC diagnostic ignored "-Wformat"
         if (sizeof(typename std::remove_pointer<T>::type) == 1) {
-            printf("\tRString[%p-%u]= %s\r\n", *buffer, *size, arg);
+            printf("\tRString[%p-%u]= %s\r\n", *buffer, size, arg);
         } else {
-            printf("\tRWString[%p-%u]= %ls\r\n", *buffer, *size, arg);
+            printf("\tRWString[%p-%u]= %ls\r\n", *buffer, size, arg);
         }
 #pragma GCC diagnostic pop
 #endif
@@ -747,7 +750,8 @@ compressSingle(BufferUtils::TwoNibbles* nibbles,
                 char **out)
 {
     if (paramType > ParamType::NON_STRING) {
-        uint32_t stringBytes = *reinterpret_cast<uint32_t*>(*in);
+        uint32_t stringBytes;
+        std::memcpy(&stringBytes, *in, sizeof(uint32_t));
         *in += sizeof(uint32_t);
 
         // Skipping strings
@@ -770,8 +774,16 @@ compressSingle(BufferUtils::TwoNibbles* nibbles,
         // to quickly skip strings in the stringsOnly=false pass.
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wpointer-arith"
-        constexpr uint32_t characterWidth =
-                                sizeof(typename std::remove_pointer<T>::type);
+        constexpr uint32_t characterWidth = [](){
+            if constexpr(std::is_same_v<std::decay_t<std::remove_pointer_t<T>>, void>)
+            {
+                return sizeof(void *);
+            }
+            else
+            {
+                return sizeof(typename std::remove_pointer<T>::type);
+            }
+        }();
 #pragma GCC diagnostic pop
 
         bzero(*out, characterWidth);
@@ -785,7 +797,8 @@ compressSingle(BufferUtils::TwoNibbles* nibbles,
         return;
     }
 
-    T argument = *reinterpret_cast<T*>(*in);
+    T argument;
+    std::memcpy(&argument, *in, sizeof(T));
 
 #ifdef ENABLE_DEBUG_PRINTING
     printf("\tCBasic  [%p->%p]= ", *in, *out);
@@ -1015,20 +1028,23 @@ log(int &logId,
                             stringSizes, args...) + sizeof(UncompressedEntry);
 
     char *writePos = NanoLogInternal::RuntimeLogger::reserveAlloc(allocSize);
-    UncompressedEntry *ue = reinterpret_cast<UncompressedEntry*>(writePos);
+    auto originalWritePos = writePos;
 
-    writePos = ue->argData;
+    UncompressedEntry entry;
+    entry.fmtId = logId;
+    entry.timestamp  = timestamp;
+    entry.entrySize = downCast<uint32_t>(allocSize);
+    std::memcpy(writePos, &entry, sizeof(entry));
+
+    writePos += sizeof(entry.fmtId) + sizeof(entry.timestamp) + sizeof(entry.entrySize);;
     store_arguments(paramTypes, stringSizes, &writePos, args...);
 
-    ue->fmtId = logId;
-    ue->timestamp = timestamp;
-    ue->entrySize = downCast<uint32_t>(allocSize);
 #ifdef ENABLE_DEBUG_PRINTING
     printf("\r\nRecording %d:'%s' of size %u\r\n",
-                        logId, info.formatString, ue->entrySize);
+                        logId, info.formatString, entry.entrySize);
 #endif
 
-    assert(allocSize == downCast<uint32_t>((writePos - (char*)(ue))));
+    assert(allocSize == downCast<uint32_t>((writePos - originalWritePos)));
     NanoLogInternal::RuntimeLogger::finishAlloc(allocSize);
 }
 
@@ -1057,9 +1073,8 @@ checkFormat(const char *, ...) {}
  *      Log arguments associated with the printf-like string.
  */
 #define NANO_LOG(severity, format, ...) do { \
-    using namespace NanoLogInternal; \
-    constexpr int numNibbles = getNumNibblesNeeded(format); \
-    constexpr int nParams = countFmtParams(format); \
+    constexpr int numNibbles = NanoLogInternal::getNumNibblesNeeded(format); \
+    constexpr int nParams = NanoLogInternal::countFmtParams(format); \
     \
     /*** Very Important*** These must be 'static' so that we can save pointers
      * to these variables and have them persist beyond the invocation.
@@ -1067,19 +1082,19 @@ checkFormat(const char *, ...) {}
      * to an expansion of #NANO_LOG) with an id and the paramTypes array is
      * used by the compression function, which is invoked in another thread
      * at a much later time. */ \
-    static constexpr std::array<ParamType, nParams> paramTypes = \
-                                analyzeFormatString<nParams>(format); \
-    static int logId = UNASSIGNED_LOGID; \
+    static constexpr std::array<NanoLogInternal::ParamType, nParams> paramTypes = \
+                                NanoLogInternal::analyzeFormatString<nParams>(format); \
+    static int logId = NanoLogInternal::UNASSIGNED_LOGID; \
     \
-    if (severity > NanoLog::getLogLevel()) \
+    if (NanoLog::severity > NanoLog::getLogLevel()) \
         break; \
     \
     /* Triggers the GNU printf checker by passing it into a no-op function.
      * Trick: This call is surrounded by an if false so that the VA_ARGS don't
      * evaluate for cases like '++i'.*/ \
-    if (false) { checkFormat(format, ##__VA_ARGS__); } \
+    if (false) { NanoLogInternal::checkFormat(format, ##__VA_ARGS__); } /*NOLINT(cppcoreguidelines-pro-type-vararg, hicpp-vararg)*/\
     \
-    NanoLogInternal::log(logId, __FILE__, __LINE__, severity, format, \
+    NanoLogInternal::log(logId, __FILE__, __LINE__, NanoLog::severity, format, \
                             numNibbles, paramTypes, ##__VA_ARGS__); \
 } while(0)
 } /* Namespace NanoLogInternal */

preprocessor/FunctionGenerator.py
@@ -198,7 +198,7 @@ namespace {{
 
 using namespace NanoLog::LogLevels;
 """.format(logLevelEnum=LOG_LEVEL_ENUM))
-            for logId, code in mergedCode.iteritems():
+            for logId, code in sorted(mergedCode.items()):
                 if logId == "__INVALID__INVALID__INVALID__":
                     continue
 
@@ -218,7 +218,7 @@ using namespace NanoLog::LogLevels;
             compressFnNameArray = []
             decompressFnNameArray = []
             dictionaryFragments = []
-            for logId, code in mergedCode.iteritems():
+            for logId, code in sorted(mergedCode.items()):
                 if logId == "__INVALID__INVALID__INVALID__":
                     continue
 
@@ -306,7 +306,7 @@ size_t numLogIds = {count};
     def getRecordFunctionDefinitionsFor(self, compilationUnit):
         recordFns = []
 
-        for logId, code in self.logId2Code.iteritems():
+        for logId, code in sorted(self.logId2Code.items()):
             if code["compilationUnit"] == compilationUnit:
                 recordFns.append(code["recordFnDef"])
 
@@ -446,7 +446,7 @@ size_t numLogIds = {count};
 
         # Bytes needed to store the primitive byte lengths
         numNibbles = len(nonStringArgsIdx)
-        nibbleByteSizes = ( numNibbles + 1)/2
+        nibbleByteSizes = int(( numNibbles + 1)/2)
 
         recordNonStringArgsCode = "".join(["\t%s(buffer, arg%d);\n" % \
                 (RECORD_PRIMITIVE_FN, idx) for idx in nonStringArgsIdx])
@@ -510,7 +510,8 @@ inline {function_declaration} {{
         readBackNonStringArgsCode = ""
         for idx in nonStringArgsIdx:
             readBackNonStringArgsCode += \
-                "\t{type} arg{id} = *reinterpret_cast<{type}*>(args); " \
+                    "\t{type} arg{id}; " \
+                    "std::memcpy(&arg{id}, args, sizeof({type})); " \
                 "args +=sizeof({type});\n".format(type=argList[idx], id=idx)
 
         packNonStringArgsCode = ""

preprocessor/UnitTests.py
@@ -218,13 +218,13 @@ class PreprocesorTestCase(unittest.TestCase):
         self.assertEqual(FilePosition(5, 8), args[3].endPos)
 
     def test_parseLogStatement_missingSemicolonOrParen(self):
-        with self.assertRaisesRegexp(ValueError, "Expected ';'"):
+        with self.assertRaisesRegex(ValueError, "Expected ';'"):
             parseLogStatement(["NANO_LOG(\"1\") }"], FilePosition(0, 0))
 
-        with self.assertRaisesRegexp(ValueError, "Expected ';'"):
+        with self.assertRaisesRegex(ValueError, "Expected ';'"):
             parseLogStatement(["NANO_LOG(\"1\")"], FilePosition(0, 0))
 
-        with self.assertRaisesRegexp(ValueError, "Cannot find end"):
+        with self.assertRaisesRegex(ValueError, "Cannot find end"):
             parseLogStatement(["NANO_LOG(\"1\""], FilePosition(0, 0))
 
     def test_peekNextMeaningfulChar(self):
@@ -291,7 +291,7 @@ class FunctionGeneratorTestCase(unittest.TestCase):
 
         # Invalid types
         fmtString = "%S %qosiwieud"
-        with self.assertRaisesRegexp(ValueError,
+        with self.assertRaisesRegex(ValueError,
                                      "Unrecognized Format Specifier"):
             splitAndParseTypesInFmtString(fmtString)
 
@@ -323,10 +323,10 @@ class FunctionGeneratorTestCase(unittest.TestCase):
         self.assertEqual(splitAndParseTypesInFmtString(" %d"),
                          [FmtType("int", None, None, " %d")])
 
-        with self.assertRaisesRegexp(ValueError, "not supported"):
+        with self.assertRaisesRegex(ValueError, "not supported"):
             splitAndParseTypesInFmtString("%hhn")
 
-        with self.assertRaisesRegexp(ValueError, "Unrecognized Format"):
+        with self.assertRaisesRegex(ValueError, "Unrecognized Format"):
             splitAndParseTypesInFmtString("%hhj")
 
     def test_parseTypesInFmtString_jzt(self):
@@ -346,19 +346,19 @@ class FunctionGeneratorTestCase(unittest.TestCase):
                           FmtType('ptrdiff_t', None, None, " %tu"),
                           FmtType("ptrdiff_t", None, None, " %td")])
 
-        with self.assertRaisesRegexp(ValueError, "specifier not supported"):
+        with self.assertRaisesRegex(ValueError, "specifier not supported"):
             splitAndParseTypesInFmtString("%jn %zn zn %tn")
 
         # Unexpected characters!
-        with self.assertRaisesRegexp(ValueError,
+        with self.assertRaisesRegex(ValueError,
                                      "Unrecognized Format Specifier: \"%z"):
             splitAndParseTypesInFmtString("%z\r\n")
 
-        with self.assertRaisesRegexp(ValueError,
+        with self.assertRaisesRegex(ValueError,
                                      "Unrecognized Format Specifier: \"%j"):
             splitAndParseTypesInFmtString("%j\r\n")
 
-        with self.assertRaisesRegexp(ValueError,
+        with self.assertRaisesRegex(ValueError,
                                      "Unrecognized Format Specifier: \"%t"):
             splitAndParseTypesInFmtString("%t\r\n")
 
@@ -391,7 +391,7 @@ class FunctionGeneratorTestCase(unittest.TestCase):
                           FmtType("double", None, None, " %llf")])
 
         # Check for errors
-        with self.assertRaisesRegexp(ValueError, "Invalid arguments for"):
+        with self.assertRaisesRegex(ValueError, "Invalid arguments for"):
             splitAndParseTypesInFmtString("%Lu")
 
     def test_parseTypesInFmtString_basicIntegerTypes(self):
@@ -410,7 +410,7 @@ class FunctionGeneratorTestCase(unittest.TestCase):
                           FmtType("const char*", None, None, " %s"),
                           FmtType("const void*", None, None, " %p")])
 
-        with self.assertRaisesRegexp(ValueError, "specifier not supported"):
+        with self.assertRaisesRegex(ValueError, "specifier not supported"):
             splitAndParseTypesInFmtString("%n")
 
     def test_parseTypesInFmtString_cspn(self):
@@ -423,7 +423,7 @@ class FunctionGeneratorTestCase(unittest.TestCase):
                          [FmtType("const wchar_t*", None, None, "%ls"),
                           FmtType("wint_t", None, None, " %lc asdf")])
 
-        with self.assertRaisesRegexp(ValueError, "not supported"):
+        with self.assertRaisesRegex(ValueError, "not supported"):
             splitAndParseTypesInFmtString("%n")
 
     def test_parseTypesInFmtString_precision(self):
@@ -457,7 +457,7 @@ class FunctionGeneratorTestCase(unittest.TestCase):
                           FmtType("size_t", None, None, " %zu"),
                           FmtType("ptrdiff_t", 9, 2, " %09.2tu")])
 
-        with self.assertRaisesRegexp(ValueError, "specifier not supported"):
+        with self.assertRaisesRegex(ValueError, "specifier not supported"):
             splitAndParseTypesInFmtString("%hhn %hn %ln %lln %jn %zn %tn")
 
     def test_generateLogFunctions_empty(self):
@@ -535,11 +535,12 @@ class FunctionGeneratorTestCase(unittest.TestCase):
         # same log, diff compilation unit, but same originating file
         fg.generateLogFunctions("DEBUG", "A", "s.cc", "mar.cc", 293)
 
-        ids = fg.logId2Code.keys()
-        self.assertEqual(['__A__mar46cc__293__',
+        ids = list(fg.logId2Code)
+        ids.sort()
+        self.assertEqual(['__A__mar46cc__200__',
+                          '__A__mar46cc__293__',
                           '__A__s46cc__100__',
                           '__B__mar46cc__293__',
-                          '__A__mar46cc__200__',
                           '__INVALID__INVALID__INVALID__'], ids)
 
     def test_getRecordFunctionDefinitionsFor(self):
@@ -586,14 +587,15 @@ inline void __syang0__fl{logId}(NanoLog::LogLevel level, const char* fmtStr ) {{
         self.assertEqual(0, len(fg.getRecordFunctionDefinitionsFor("asdf.cc")))
 
         funcs = fg.getRecordFunctionDefinitionsFor("mar.cc")
+        funcs.sort()
 
         logId = generateLogIdStr("A", "mar.cc", 293)
         self.assertMultiLineEqual(emptyRec.format(logId=logId), funcs[0])
 
-        logId = generateLogIdStr("C", "mar.cc", 200)
+        logId = generateLogIdStr("B", "mar.cc", 293)
         self.assertMultiLineEqual(emptyRec.format(logId=logId), funcs[1])
 
-        logId = generateLogIdStr("B", "mar.cc", 293)
+        logId = generateLogIdStr("C", "mar.cc", 200)
         self.assertMultiLineEqual(emptyRec.format(logId=logId), funcs[2])
 
         logId = generateLogIdStr("D", "mar.cc", 100)

preprocessor/parser.py
@@ -299,7 +299,7 @@ def peekNextMeaningfulChar(lines, filePos):
     line = lines[lineNum]
     while offset < len(line):
       c = line[offset]
-      if isprintable(c) and not c.isspace():
+      if not c.isspace():
         return (c, FilePosition(lineNum, offset))
       offset = offset + 1
     offset = 0
@@ -552,8 +552,8 @@ def processFile(inputFile, mapOutputFilename):
 
           lastChar = c
     except ValueError as e:
-        print "\r\n%s:%d: Error - %s\r\n\r\n%s\r\n" % (
-            ppFileName, ppLineNum, e.args[0], "".join(e.args[1]))
+        print("\r\n%s:%d: Error - %s\r\n\r\n%s\r\n" % (
+            ppFileName, ppLineNum, e.args[0], "".join(e.args[1])))
         sys.exit(1)
 
     # Last step, retrieve the generated code and insert it at the end

runtime/Log.cc
@@ -60,6 +60,7 @@ Log::insertCheckpoint(char **out, char *outLimit, bool writeDictionary) {
     if (!writeDictionary)
         return true;
 
+#ifdef PREPROCESSOR_NANOLOG
     long int bytesWritten = GeneratedFunctions::writeDictionary(*out, outLimit);
 
     if (bytesWritten == -1) {
@@ -72,6 +73,7 @@ Log::insertCheckpoint(char **out, char *outLimit, bool writeDictionary) {
     ck->newMetadataBytes = static_cast<uint32_t>(bytesWritten);
     ck->totalMetadataEntries = static_cast<uint32_t>(
             GeneratedFunctions::numLogIds);
+#endif // PREPROCESSOR_NANOLOG
 
     return true;
 }
@@ -181,6 +183,7 @@ Log::Encoder::encodeNewDictionaryEntries(uint32_t& currentPosition,
     return df->newMetadataBytes;
 }
 
+#ifdef PREPROCESSOR_NANOLOG
 /**
  * Interprets the uncompressed log messages (created by the compile-time
  * generated code) contained in the *from buffer and compresses them to
@@ -259,13 +262,17 @@ Log::Encoder::encodeLogMsgs(char *from,
     }
 
     assert(currentExtentSize);
-    *currentExtentSize += downCast<uint32_t>(writePos - bufferStart);
+    uint32_t currentSize;
+    std::memcpy(&currentSize, currentExtentSize, sizeof(uint32_t));
+    currentSize += downCast<uint32_t>(writePos - bufferStart);
+    std::memcpy(currentExtentSize, &currentSize, sizeof(uint32_t));
 
     if (numEventsCompressed)
         *numEventsCompressed += numEventsProcessed;
 
     return nbytes - remaining;
 }
+#endif // PREPROCESSOR_NANOLOG
 
 
 /**
@@ -325,10 +332,8 @@ Log::Encoder::encodeLogMsgs(char *from,
                 fprintf(stderr, "NanoLog Error: Metadata missing for a dynamic "
                                 "log message (id=%u) during compression. If "
                                 "you are using Preprocessor NanoLog, there is "
-                                "be a problem with your integration (static "
-                                "logs detected=%lu).\r\n",
-                                entry->fmtId,
-                                GeneratedFunctions::numLogIds);
+                                "be a problem with your integration.\r\n",
+                                entry->fmtId);
             }
 
             break;
@@ -383,7 +388,10 @@ Log::Encoder::encodeLogMsgs(char *from,
     }
 
     assert(currentExtentSize);
-    *currentExtentSize += downCast<uint32_t>(writePos - bufferStart);
+    uint32_t currentSize;
+    std::memcpy(&currentSize, currentExtentSize, sizeof(uint32_t));
+    currentSize += downCast<uint32_t>(writePos - bufferStart);
+    std::memcpy(currentExtentSize, &currentSize, sizeof(uint32_t));
 
     if (numEventsCompressed)
         *numEventsCompressed += numEventsProcessed;
@@ -1347,6 +1355,7 @@ Log::Decoder::BufferFragment::decompressNextLogStatement(FILE *outputFd,
         strftime(timeString, sizeof(timeString), "%Y-%m-%d %H:%M:%S", tm);
     }
 
+#ifdef PREPROCESSOR_NANOLOG
     if (fmtId2metadata.empty() || aggregationFn != nullptr) {
         // Output the context
         struct GeneratedFunctions::LogMetadata meta =
@@ -1379,7 +1388,9 @@ Log::Decoder::BufferFragment::decompressNextLogStatement(FILE *outputFd,
         GeneratedFunctions::decompressAndPrintFnArray[nextLogId](&readPos,
                                                                  outputFd,
                                                                  aggFn);
-    } else {
+    } else 
+#endif // PREPROCESSOR_NANOLOG
+    {
         using namespace BufferUtils;
         auto *metadata = reinterpret_cast<FormatMetadata*>(
                                             fmtId2metadata.at(nextLogId));

runtime/Log.h
@@ -638,10 +638,12 @@ namespace Log {
                 bool skipCheckpoint=false,
                 bool forceDictionaryOutput=false);
 
+#ifdef PREPROCESSOR_NANOLOG
         long encodeLogMsgs(char *from, uint64_t nbytes,
                            uint32_t bufferId,
                            bool wrapAround,
                            uint64_t *numEventsCompressed);
+#endif // PREPROCESSOR_NANOLOG
 
         long encodeLogMsgs(char *from, uint64_t nbytes,
                                     uint32_t bufferId,
@@ -675,7 +677,7 @@ namespace Log {
 
         // A pointer to the last encoded BufferExtent's length to allow updating
         // the value as the user performs more encodeLogMsgs with the same id.
-        uint32_t *currentExtentSize;
+        void *currentExtentSize;
 
         // Metric: Total number of encode failures due to missing metadata. This
         // is typically due to a benign race condition, but could indicate an

runtime/LogDecompressor.cc
@@ -30,6 +30,8 @@
 
 using namespace NanoLogInternal::Log;
 
+
+#ifdef PREPROCESSOR_NANOLOG
 /**
  * Find all the original NANO_LOG format strings in the user sources that
  * statically contain the searchString and print them out in the format
@@ -63,6 +65,7 @@ printLogMetadataContainingSubstring(std::string searchString)
                                                                 lm.fmtString);
     }
 }
+#endif // PREPROCESSOR_NANOLOG
 
 /**
  * Produces a GNUPlot graphable reverse CDF graph to stdout given a vector of
@@ -130,6 +133,7 @@ void printHelp(const char *exe) {
     printf("when there is one runtime logging thread:\r\n");
     printf("\t%s rcdfTime <logFile>\r\n\r\n", exe);
 
+#ifdef PREPROCESSOR_NANOLOG
     printf("== Note ==\r\n");
     printf("The following 2 commands only work with logs produced by the\r\n");
     printf("preprocessor version of NanoLog\r\n");
@@ -142,6 +146,7 @@ void printHelp(const char *exe) {
     printf("Get the logIds with static log format messages "
             "matching a substring (case sensitive)\r\n");
     printf("\t%s find <logFile> <substring>\r\n\r\n", exe);
+#endif
 }
 
 /**
@@ -170,7 +175,9 @@ int main(int argc, char** argv) {
         outputFd = stdout;
     }  else if (strcmp(command, "rcdfTime") == 0) {
         doRCDF = true;
-    } else if (strcmp(command, "minMaxMean") == 0) {
+    } 
+#ifdef PREPROCESSOR_NANOLOG
+    else if (strcmp(command, "minMaxMean") == 0) {
         if (argc < 4) {
             printHelp(argv[0]);
             exit(1);
@@ -197,7 +204,9 @@ int main(int argc, char** argv) {
         }
 
         find = true;
-    } else {
+    } 
+#endif // PREPROCESSOR_NANOLOG
+    else {
         printHelp(argv[0]);
         exit(1);
     }
@@ -209,7 +218,9 @@ int main(int argc, char** argv) {
     }
 
     if (find) {
+#ifdef PREPROCESSOR_NANOLOG
         printLogMetadataContainingSubstring(argv[3]);
+#endif // PREPROCESSOR_NANOLOG
         return 0;
     }
 
@@ -305,9 +316,11 @@ int main(int argc, char** argv) {
     uint64_t stop = PerfUtils::Cycles::rdtsc();
     double time = PerfUtils::Cycles::toSeconds(stop - start);
 
+#ifdef PREPROCESSOR_NANOLOG
     printf("Aggregating log message \"%s\" (logId=%d)\r\n",
                 GeneratedFunctions::logId2Metadata[filterId].fmtString,
                 filterId);
+#endif // PREPROCESSOR_NANOLOG
     printf("Logs Encountered: %lu\r\n", numLogs);
     printf("Matching Logs: %ld (%0.2lf%%)\r\n", count,
             (100.0*(double)count)/(double)numLogs);

commit 14554afbd6137e996242f950b4b22b710140fb42

Fixed a performance regression in C++17 NanoLog
Commit c6efd37 introduced a performance regression in C++17 NanoLog
where the median log latency was increased from ~8ns to ~12ns. This
was caused by a structure (UncompressedEntry) being constructed on the
stack and then memcpy'd into the staging buffer. This commit fixes it
by using placement new to directly allocate the structure in the buffer.

runtime/NanoLogCpp17.h
@@ -1030,18 +1030,18 @@ log(int &logId,
     char *writePos = NanoLogInternal::RuntimeLogger::reserveAlloc(allocSize);
     auto originalWritePos = writePos;
 
-    UncompressedEntry entry;
-    entry.fmtId = logId;
-    entry.timestamp  = timestamp;
-    entry.entrySize = downCast<uint32_t>(allocSize);
-    std::memcpy(writePos, &entry, sizeof(entry));
+    UncompressedEntry *ue = new(writePos) UncompressedEntry();
+    writePos += sizeof(UncompressedEntry);
 
-    writePos += sizeof(entry.fmtId) + sizeof(entry.timestamp) + sizeof(entry.entrySize);;
     store_arguments(paramTypes, stringSizes, &writePos, args...);
+
+    ue->fmtId = logId;
+    ue->timestamp = timestamp;
+    ue->entrySize = downCast<uint32_t>(allocSize);
 
 #ifdef ENABLE_DEBUG_PRINTING
     printf("\r\nRecording %d:'%s' of size %u\r\n",
-                        logId, info.formatString, entry.entrySize);
+                        logId, info.formatString, ue->entrySize);
 #endif
 
     assert(allocSize == downCast<uint32_t>((writePos - originalWritePos)));